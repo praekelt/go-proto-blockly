624abd71dc79630e276bad3a502ad635246fbca7
justinvdm <justinvdm.0.1.0@gmail.com>
2014-06-17 16:07:53 +0200

    Rebuild

--- go-proto-blockly.js
+++ go-proto-blockly.js
@@ -2,7 +2,23 @@ window.goBlockly = {};
 
 goBlockly.blocks = {};
 
-goBlockly.blocks.textLength = function() {
-    return {
+(function() {
+    Blockly.JavaScript.textLength = function(block) {
+        var arg0 = Blockly.JavaScript.valueToCode(
+            block, 'VALUE', Blockly.JavaScript.ORDER_FUNCTION_CALL) || "''";
+        return [arg0 + '.length', Blockly.JavaScript.ORDER_MEMBER];
     };
-}();
+})();
+
+(function() {
+    Blockly.Blocks.textLength = {
+        init: function() {
+            this.appendValueInput('VALUE')
+                .setCheck('String')
+                .appendField('length');
+
+            this.setOutput(true, 'Number');
+            this.setTooltip('Returns number of letters in the provided text.');
+        }
+    };
+})();
