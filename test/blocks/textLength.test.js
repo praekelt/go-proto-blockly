e709253d1296b5903f3a26228adf945ebcf5c483
justinvdm <justinvdm.0.1.0@gmail.com>
2014-06-17 16:07:34 +0200

    Add a basic test for textLength block

--- test/blocks/textLength.test.js
+++
@@ -1,7 +0,0 @@
-describe("goBlockly.blocks.textLength", function() {
-    describe(".foo", function() {
-        it("should return 1", function() {
-            assert.equal(goBlockly.blocks.textLength.foo(), 1);
-        });
-    });
-});
---
+++ test/blocks/textLength/js.test.js
@@ -0,0 +1,11 @@
+describe("goBlockly:blocks:textLength:js", function() {
+    it("should generate a text length lookup", function() {
+        var text = Blockly.Block.obtain(Blockly.mainWorkspace, 'text');
+        text.setFieldValue('foo', 'TEXT');
+
+        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'textLength');
+        block.getInput('VALUE').connection.connect(text.outputConnection);
+
+        assert.equal(Blockly.JavaScript.workspaceToCode(), "'foo'.length;\n");
+    });
+});
