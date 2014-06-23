b5c8163d68c983c85a41ddddd5c983c348305754
Merada Richter <merada.richter@gmail.com>
2014-06-23 13:23:25 +0200

    Repairs tests to create divs before each test and not globally.

--- test/blocks/simpleState/js.test.js
+++ test/blocks/simpleState/js.test.js
@@ -1,15 +1,15 @@
-var el;
-
-beforeEach(function () {
-	el = document.createElement("div");
-	Blockly.inject(document.body.appendChild(el));
-});
-
-afterEach(function () {
-	document.body.removeChild(el);
-});
-
 describe("goBlockly:blocks:simpleState:js", function() {
+    var el;
+
+    beforeEach(function () {
+        el = document.createElement("div");
+        Blockly.inject(document.body.appendChild(el));
+    });
+
+    afterEach(function () {
+        document.body.removeChild(el);
+    });
+
     it("should generate a text length lookup", function() {
         var text = Blockly.Block.obtain(Blockly.mainWorkspace, 'text');
         text.setFieldValue('foo', 'TEXT');
