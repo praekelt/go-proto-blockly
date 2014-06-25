ac9915d194bfd8b63afbad6cd36eeb10368b8149
Merada Richter <merada.richter@gmail.com>
2014-06-25 16:03:42 +0200

    Adds tooltip for accept_labels opt

--- src/blocks/opts/accept_labels/view.js
+++ src/blocks/opts/accept_labels/view.js
@@ -9,7 +9,7 @@
             this.setInputsInline(true);
             this.setPreviousStatement(true, ["error", "accept_labels", "send_reply", "continue_session", "events"]);
             this.setNextStatement(true, ["error", "accept_labels", "send_reply", "continue_session", "events"]);
-            this.setTooltip('');
+            this.setTooltip('Whether choice labels are accepted as the user’s responses. For eg, if accept_labels is true, the state will accepts both “1” and “Red” as responses responses if the state’s first choice is “Red”. Defaults to false.');
         }
     };
 })();
