0875d77f5b466beb23dd60eb870a3ee62adc2172
Merada Richter <merada.richter@gmail.com>
2014-06-25 14:42:02 +0200

    Fixes indentation.

--- src/blocks/states/simpleState/view.js
+++ src/blocks/states/simpleState/view.js
@@ -1,19 +1,19 @@
 (function() {
-    Blockly.Blocks.simpleState = {
-        init: function() {
-		    this.setColour(230);
-		    this.appendDummyInput()
-		        .appendField("SIMPLE STATE");
-		    this.appendValueInput("NAME")
-		        .setCheck("String")
-		        .appendField("Name");
-		    this.appendStatementInput("CHOICES")
-		        .setCheck("choice")
-		        .appendField("Choices");
-		    this.appendValueInput("NEXT")
-		        .setCheck("String") // would ideally be a drop down menu or function of some kind
-		        .appendField("Next state");
-		    this.setTooltip('A simple state containing any number of choices');
-        }
-    };
+	Blockly.Blocks.simpleState = {
+		init: function() {
+			this.setColour(230);
+			this.appendDummyInput()
+				.appendField("SIMPLE STATE");
+			this.appendValueInput("NAME")
+				.setCheck("String")
+				.appendField("Name");
+			this.appendStatementInput("CHOICES")
+				.setCheck("choice")
+				.appendField("Choices");
+			this.appendValueInput("NEXT")
+				.setCheck("String") // would ideally be a drop down menu or function of some kind
+				.appendField("Next state");
+			this.setTooltip('A simple state containing any number of choices');
+		}
+	};
 })();
