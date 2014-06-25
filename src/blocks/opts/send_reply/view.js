89a3c705eb58afac45550962f008234b10da78c4
Merada Richter <merada.richter@gmail.com>
2014-06-25 16:00:24 +0200

    Adds tooltip for send_reply opt

--- src/blocks/opts/send_reply/view.js
+++ src/blocks/opts/send_reply/view.js
@@ -9,7 +9,7 @@
             this.setInputsInline(true);
             this.setPreviousStatement(true, ["error", "accept_labels", "send_reply", "continue_session", "events"]);
             this.setNextStatement(true, ["error", "accept_labels", "send_reply", "continue_session", "events"]);
-            this.setTooltip('');
+            this.setTooltip('Whether or not a reply should be sent to the user’s message. Default is true.');
         }
     };
 })();
