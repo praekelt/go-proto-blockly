f62b7b1c19c47ac02602fb8c3f6621ac18f53a83
Merada Richter <merada.richter@gmail.com>
2014-06-25 16:01:57 +0200

    Adds tooltip for error opt

--- src/blocks/opts/error/view.js
+++ src/blocks/opts/error/view.js
@@ -9,7 +9,7 @@
             this.setInputsInline(true);
             this.setPreviousStatement(true, ["error", "accept_labels", "send_reply", "continue_session", "events"]);
             this.setNextStatement(true, ["error", "accept_labels", "send_reply", "continue_session", "events"]);
-            this.setTooltip('');
+            this.setTooltip('Error text to display to the user if bad user input was given.');
         }
     };
 })();
