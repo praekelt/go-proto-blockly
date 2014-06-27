8166fb6978d179a5bd48f61ef04ff41814b680e8
Merada Richter <merada.richter@gmail.com>
2014-06-27 12:41:41 +0200

    Adds Handlebars as dependency to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
 
     grunt.initConfig({
         paths: {
