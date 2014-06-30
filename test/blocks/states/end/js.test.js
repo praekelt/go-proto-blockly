eef02ea3753b07acb1f04efa8a36d8588571a915
Merada Richter <merada.richter@gmail.com>
2014-06-30 15:59:18 +0200

    Refactors end state test statment

--- test/blocks/states/end/js.test.js
+++ test/blocks/states/end/js.test.js
@@ -11,18 +11,18 @@ describe("goBlockly:blocks:states:end:js", function() {
     });
 
     it("should generate an End State", function() {
+        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'state_end');
+
         var value_name = Blockly.Block.obtain(Blockly.mainWorkspace, 'text');
         value_name.setFieldValue('name', 'TEXT');
+        block.getInput('NAME').connection.connect(value_name.outputConnection);
         
         var value_text = Blockly.Block.obtain(Blockly.mainWorkspace, 'text');
         value_text.setFieldValue('text', 'TEXT');
+        block.getInput('TEXT').connection.connect(value_text.outputConnection);
         
         var value_next = Blockly.Block.obtain(Blockly.mainWorkspace, 'text');
         value_next.setFieldValue('next', 'TEXT');
-        
-        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'state_end');
-        block.getInput('NAME').connection.connect(value_name.outputConnection);
-        block.getInput('TEXT').connection.connect(value_text.outputConnection);
         block.getInput('NEXT').connection.connect(value_next.outputConnection);
 
         assert.equal(Blockly.JavaScript.workspaceToCode(), [
