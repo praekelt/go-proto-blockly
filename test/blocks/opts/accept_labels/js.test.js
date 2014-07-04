e1ddf5634d6042995d442bc64e729109d7d4ab99
Merada Richter <merada.richter@gmail.com>
2014-07-04 10:31:43 +0200

    Adds accept_labels test

--- test/blocks/opts/accept_labels/js.test.js
+++ test/blocks/opts/accept_labels/js.test.js
@@ -1,1 +1,21 @@
-// no functionality yet
+describe("goBlockly:blocks:opts:accept_labels:js", function() {
+    var el;
+
+    beforeEach(function () {
+        el = document.createElement("div");
+        Blockly.inject(document.body.appendChild(el));
+    });
+
+    afterEach(function () {
+        document.body.removeChild(el);
+    });
+
+    it("should generate an Accept_Labels opt", function() {
+    	var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'logic_boolean');
+        
+    	var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'opts_accept_labels');
+        block.getInput('VALUE').connection.connect(bool.outputConnection);
+
+        assert.equal(Blockly.JavaScript.workspaceToCode(), "accept_labels: true");
+    });
+});
