34762f18fab49e4e796a22e671247aab0af00332
Merada Richter <merada.richter@gmail.com>
2014-07-04 12:15:36 +0200

    Adds test for continue_session

--- test/blocks/opts/continue_session/js.test.js
+++ test/blocks/opts/continue_session/js.test.js
@@ -1,1 +1,21 @@
-// no functionality yet
+describe("goBlockly:blocks:opts:continue_session:js", function() {
+    var el;
+
+    beforeEach(function () {
+        el = document.createElement("div");
+        Blockly.inject(document.body.appendChild(el));
+    });
+
+    afterEach(function () {
+        document.body.removeChild(el);
+    });
+
+    it("should generate a Continue_Session opt", function() {
+        var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'logic_boolean');
+        
+        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'opts_continue_session');
+        block.getInput('VALUE').connection.connect(bool.outputConnection);
+
+        assert.equal(Blockly.JavaScript.workspaceToCode(), "continue_session: true");
+    });
+});
