670f1ca1a08865dbd45f0d91b085ddb7b1ac9567
Merada Richter <merada.richter@gmail.com>
2014-07-04 12:20:52 +0200

    Adds test statements (true and false)

--- test/blocks/opts/continue_session/js.test.js
+++ test/blocks/opts/continue_session/js.test.js
@@ -10,12 +10,22 @@ describe("goBlockly:blocks:opts:continue_session:js", function() {
         document.body.removeChild(el);
     });
 
-    it("should generate a Continue_Session opt", function() {
+    it("should generate a continue_session opt set to true", function() {
         var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'logic_boolean');
         
         var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'opts_continue_session');
         block.getInput('VALUE').connection.connect(bool.outputConnection);
 
         assert.equal(Blockly.JavaScript.workspaceToCode(), "continue_session: true");
+    });
+
+    it("should generate a continue_session opt set to false", function() {
+        var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'logic_boolean');
+        bool.setFieldValue('false', 'BOOL');
+        
+        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'opts_continue_session');
+        block.getInput('VALUE').connection.connect(bool.outputConnection);
+
+        assert.equal(Blockly.JavaScript.workspaceToCode(), "continue_session: false");
     });
 });
