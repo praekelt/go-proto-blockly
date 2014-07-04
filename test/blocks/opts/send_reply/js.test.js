8b95286796ac33a58788e803ea65d8d469faf08f
Merada Richter <merada.richter@gmail.com>
2014-07-04 14:20:10 +0200

    Adds test for send_reply

--- test/blocks/opts/send_reply/js.test.js
+++ test/blocks/opts/send_reply/js.test.js
@@ -1,1 +1,31 @@
-// no functionality yet
+describe("goBlockly:blocks:opts:send_reply:js", function() {
+    var el;
+
+    beforeEach(function () {
+        el = document.createElement("div");
+        Blockly.inject(document.body.appendChild(el));
+    });
+
+    afterEach(function () {
+        document.body.removeChild(el);
+    });
+
+    it("should generate a send_reply opt set to true", function() {
+        var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'logic_boolean');
+        
+        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'opts_send_reply');
+        block.getInput('VALUE').connection.connect(bool.outputConnection);
+
+        assert.equal(Blockly.JavaScript.workspaceToCode(), "send_reply: true");
+    });
+
+    it("should generate a send_reply opt set to false", function() {
+        var bool = Blockly.Block.obtain(Blockly.mainWorkspace, 'logic_boolean');
+        bool.setFieldValue('false', 'BOOL');
+        
+        var block = Blockly.Block.obtain(Blockly.mainWorkspace, 'opts_send_reply');
+        block.getInput('VALUE').connection.connect(bool.outputConnection);
+
+        assert.equal(Blockly.JavaScript.workspaceToCode(), "send_reply: false");
+    });
+});
